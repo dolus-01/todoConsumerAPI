ff59ca5624450623636f8a13843e50014d44947b
dolus-01 <145191392+dolus-01@users.noreply.github.com>
2025-01-30 13:09:20 +0800

    added controller test

--- src/test/java/com/todo/consumer/controllers/TodoControllerTest.java
+++ src/test/java/com/todo/consumer/controllers/TodoControllerTest.java
@@ -2,58 +2,71 @@ package com.todo.consumer.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
 
 import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.todo.consumer.ConsumerApplication;
+import com.todo.consumer.model.response.TodoResponse;
 import com.todo.consumer.service.TodoService;
 
-@SpringBootTest(classes = { ConsumerApplication.class })
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TodoControllerTest {
-
+	@Autowired
 	private MockMvc mvc;
 
 	@Value("${server.servlet.context-path}")
 	private String contextPath;
 
-	@Mock
+	@SuppressWarnings("removal")
+	@MockBean
 	private TodoService todoService;
-
-	@InjectMocks
-	private TodoController todoController;
+	
+	private List<TodoResponse> listResponse;
+	
+	private TodoResponse todoResponse;
 
 	@BeforeEach
 	public void setup() {
-		mvc = MockMvcBuilders.standaloneSetup(todoController).build();
+		todoResponse = new TodoResponse(); 
+		todoResponse.setCompleted(false);
+		todoResponse.setId(1);
+		todoResponse.setTitle("testTItle");
+		todoResponse.setUserId(1);
+		List<TodoResponse> listResponse = new LinkedList<TodoResponse>();
+		listResponse.add(todoResponse);
 	}
 
 	@Test
 	public void TestGetAllData() throws Exception {
-		lenient().when(todoService.getAllJsonData()).thenReturn(new Object());
-		mvc.perform(get("/dev/todo/json/data").contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().is4xxClientError());
+		given(todoService.getAllJsonData()).willReturn((Object)listResponse);
+		mvc.perform(get("/json/data").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
 
 	}
 
 	@Test
 	public void TestGetData() throws Exception {
-		lenient().when(todoService.getJsonData("1")).thenReturn(new Object());
-//		given(todoService.getJsonData("1")).willReturn(new Object());
-		mvc.perform(get(contextPath.concat("/json/data/{id}"), "1").contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().is4xxClientError());
+		given(todoService.getJsonData("1")).willReturn(todoResponse);
+		mvc.perform(get("/json/data/{id}","1").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
 	}
 
 }
